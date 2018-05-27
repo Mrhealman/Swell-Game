19c37af156d045e05be6a433802dcb8c6702d1b7
Mr_healman <sdan02@yandex.ru>
2018-05-27 23:01:59 +0300

    Swell_Game version 0.02

---
+++ Swell_Game_V0.02/Swell_Game_V0.02/Game_Lvl1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swell_Game_V0._02
+{
+    static partial class Game_Lvl1
+    {
+        
+    }
+}
